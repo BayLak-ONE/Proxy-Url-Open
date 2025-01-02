c9df6e9fc53b06350b76a790f45f150f2faec3ee
BayLak-ONE <mh1231231234go@gmail.com>
2025-01-03 01:10:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-echo Installing true.. && url
+echo Installing true.. 
 chmod +x $(pwd)/url
 chmod +x $(pwd)/EcroxyV1
 chmod +x $(pwd)/EcroxyV2
 sudo update-alternatives --install /usr/bin/urls urls $(pwd)/url 1
 sudo update-alternatives --install /usr/bin/EcroxyV1 EcrxoyV1 $(pwd)/EcroxyV1 1
 sudo update-alternatives --install /usr/bin/EcroxyV2 EcrxoyV2 $(pwd)/EcroxyV1 1
+url
